fd42cbad2fc210ba6d941050297636e86ac3eddf
Ana Sato <anasato.est@gmail.com>
2023-10-14 15:03:53 -0300

    Content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Saiu Nova Versao do Angular",
+    "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum aut molestiae voluptas vel! Ab tempore ratione nulla doloribus eaque. Suscipit sapiente alias repudiandae facere. Nemo sit eligendi quas voluptates possimus.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum aut molestiae voluptas vel! Ab tempore ratione nulla doloribus eaque. Suscipit sapiente alias repudiandae facere. Nemo sit eligendi quas voluptates possimus.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum aut molestiae voluptas vel! Ab tempore ratione nulla doloribus eaque. Suscipit sapiente alias repudiandae facere. Nemo sit eligendi quas voluptates possimus.",
+    "photoCover":"https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
+  },
+
+  {
+    "id":"2",
+    "title":"Saiu Nova Versao do Angular",
+    "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum aut molestiae voluptas vel! Ab tempore ratione nulla doloribus eaque. Suscipit sapiente alias repudiandae facere. Nemo sit eligendi quas voluptates possimus.",
+    "photoCover":"https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
+  },
+
+  {
+    "id":"3",
+    "title":"Nova noticia",
+    "description":"hnieggikgj",
+    "photoCover":"https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
+  },
+
+  {
+    "id":"4",
+    "title":"Minha Noticia",
+    "description":"Ola, mundo! Estou aprendendo Angular com a DIO",
+    "photoCover":"https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
+  }
+]
